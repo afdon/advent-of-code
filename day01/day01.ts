d093114cff7deea121daf8dbe9b1b58441a8c824
afdon <53676753+afdon@users.noreply.github.com>
2023-10-04 18:09:16 -0700

    part 1 complete

--- day01/day01.ts
+++ day01/day01.ts
@@ -1,14 +1,58 @@
-const fs = require('fs');
+import fs from "fs";
+
+let input
 
 // Read the input file asynchronously
+
 fs.readFile('input.txt', 'utf8', (err, data) => {
     if (err) {
         console.error('Error reading input file:', err);
         return;
     }
 
-    // format the data
-    // const formattedData = formatData(data);
-    console.log(data);
+    part1(data);
 });
+
+
+function part1(input: string) {
+    const lines = input.split("\n")
+    // .slice(0,25)
+    const groups = []
+
+    let curGroup:number = 0
+
+    for (let i = 0; i < lines.length; i++) {
+        if (lines[i] !== "") {
+            if (!groups[curGroup]) {
+                groups[curGroup] = []
+            }
+            groups[curGroup].push(parseInt(lines[i]))
+        } else {
+            curGroup++
+        }
+    }
+    // console.log(groups)
+
+    const summedGroups = groups.map(group => {
+        let sum = 0
+        for (let i = 0; i < group.length; i++) {
+            sum = sum + group[i]
+        }
+        return sum
+    })
+
+    console.log(summedGroups)
+
+    const sortFunction = (a: number, b: number) => {
+        if (a > b) return -1
+    }
+
+    const sumsAscending = summedGroups.sort(sortFunction)
+
+    // console.log(sumsAscending)
+
+    console.log(sumsAscending[0])
+
+    return sumsAscending[0]
+}
 
