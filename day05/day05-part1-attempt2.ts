02daa9533b8338ae287cd36757f93f70c3c38d91
afdon <53676753+afdon@users.noreply.github.com>
2023-10-17 13:28:23 -0700

    redo createRepresentation

---
+++ day05/day05-part1-attempt2.ts
@@ -0,0 +1,77 @@
+
+
+import fs from "fs";
+
+fs.readFile("input.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading input file:", err);
+    return;
+  }
+
+  part1(data);
+});
+
+function part1(input: string) {
+//   const lines = input.split("\n");
+
+    const initialState = input.split("\n").slice(0, 10);
+    // console.log("initial state: ", initialState)
+
+    const instructions = input.split("\n").slice(10)
+    // console.log("instructions: ", instructions)
+
+    // console.log("initialState's type is:", typeof initialState)
+    const numOfStacks = getNumberOfStacks(initialState)
+    // console.log ("number of stacks:", numOfStacks)
+
+    const representation = createRepresentation(initialState)
+    console.log("representation", representation)
+
+    let task = {
+        "number of stacks": numOfStacks,
+        "initialState": initialState,
+        "instructions": instructions
+    }
+
+    console.log("task:", task)
+}
+
+function createRepresentation(initial) {
+    // const numOfStacks = getNumberOfStacks(initial)
+
+    const linesToProcess = initial.slice(0, -2) // exclude the stack line and the empty line
+
+    // go through each line, starting from the end.
+
+    let boxesArray = new Array(getNumberOfStacks(initial))
+
+    for (let i = linesToProcess.length - 1; i <= 0; i--) {
+
+}
+}
+
+//// wip
+
+function getCharPositions(line: string) {
+    let charIndices: any[] = []
+    line.split("").map(char => {
+        if (isNaN(parseInt(char))) {
+            console.log("This char is not a number.", char)
+        } else {
+            let position = line.indexOf(char)
+            charIndices.push(position)
+        }
+    })
+}
+
+//// helper functions
+
+function isLetter(str) {
+    if (str.match(/[a-z]/i)) return str
+}
+
+function getNumberOfStacks(initial) {
+    const stacksArray = initial[initial.length - 2].trim().split("   ") // trim; split by 3 spaces
+    console.log("stacks array", stacksArray)
+    return stacksArray.length
+}
