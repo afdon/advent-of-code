b3be3d8573cf229e4362e80dbde0cb9893e55132
afdon <53676753+afdon@users.noreply.github.com>
2023-10-17 14:51:59 -0700

    archive old

---
+++ day05/day05-part1-attempt4.ts
@@ -0,0 +1,94 @@
+import fs from "fs";
+
+fs.readFile("input.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading input file:", err);
+    return;
+  }
+
+  part1(data);
+});
+
+function part1(input: string) {
+  const initialState = input.split("\n").slice(0, 10);
+
+  const instructions = input.split("\n").slice(10);
+
+  const stackLine = initialState[initialState.length - 2];
+//   console.log("stackLine:", stackLine);
+  const charPositions = getCharPositions(stackLine);
+//   console.log(`charPositions:`, charPositions);
+
+  const numOfStacks = getNumberOfStacks(initialState);
+
+  const representation: string[][] = createRepresentation(initialState);
+  printRepresentation(representation)
+
+}
+
+
+//// wip
+
+
+
+//// helper functions
+
+function printRepresentation(rep: string[][]) {
+    rep.map((e: string[], i: number) => {
+        console.log(`this is index ${i}, stack #${i + 1}:`, e)
+    })
+}
+
+
+function createRepresentation(initial) {
+  const linesToProcess = initial.slice(0, -2); // exclude stack line and empty line
+
+  let boxesArray: any[][] = [];
+  for (let i = 0; i < getNumberOfStacks(initial); i++) {
+    boxesArray.push([]);
+  } 
+
+  for (let i = linesToProcess.length - 1; i >= 0; i--) { // iterate from the end
+    let clean = cleanLine(linesToProcess[i]);
+    for (let j = 0; j < clean.length; j++) {
+      boxesArray[j].push(clean[j]);
+    }
+  }
+  return boxesArray
+}
+
+function cleanLine(line: string) {
+  let cleanedLine = line
+    .split("")
+    .map((char, i) => {
+      if (charPositions.includes(i)) {
+        return char;
+      }
+    })
+    .filter((char) => char !== undefined);
+  return cleanedLine;
+}
+
+let charPositions: number[];
+function getCharPositions(line: string) {
+  let charIndices: number[] = [];
+
+  line.split("").map((char) => {
+    if (isNaN(parseInt(char))) {
+    } else {
+      let index: number = line.indexOf(char);
+      charIndices.push(index);
+    }
+  });
+  charPositions = charIndices;
+  return charIndices;
+}
+
+function isLetter(str) {
+  if (str.match(/[a-z]/i)) return str;
+}
+
+function getNumberOfStacks(initial) {
+  const stacksArray = initial[initial.length - 2].trim().split("   "); // trim; split by 3 spaces
+  return stacksArray.length;
+}
