7ca67fc96e55fb2fbf875e9b91a518b27266c353
afdon <53676753+afdon@users.noreply.github.com>
2023-10-17 10:51:42 -0700

    get stacks array done

--- day05/day05-part1-attempt1.ts
+++ day05/day05-part1-attempt1.ts
@@ -20,5 +20,17 @@ function part1(input: string) {
     const instructions = input.split("\n").slice(10)
     console.log("instructions: ", instructions)
 
+    console.log("initialState's type is:", typeof initialState)
+    getNumberOfStacks(initialState)
+
 }
 
+function getNumberOfStacks(initial) {
+    const stacksArray = initial[initial.length - 2].trim().split("   ") // trim; split by 3 spaces
+    console.log("stacks array", stacksArray)
+}
+
+function createRepresentation(initial) {
+    // grab the last line.
+
+}
