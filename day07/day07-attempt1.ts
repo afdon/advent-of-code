6fc08ca1ab75cf06b5de35fa914795e4d3c308f2
afdon <53676753+afdon@users.noreply.github.com>
2023-10-18 14:33:30 -0700

    string matching function wip

--- day07/day07-attempt1.ts
+++ day07/day07-attempt1.ts
@@ -17,15 +17,28 @@ function part1(input: string) {
 }
 
 function validate(string: string) {
-    switch (string) {
-        case value1:
-          // command is a 
-        case value2:
-          statements
-        // …
-        case valueN:
-          statements
-        default:
-          statements
+
+  if (string.charAt(0) === "$") {
+    if (string.charAt(2) === "c" && string.charAt(3) == "d") {
+      if (string.charAt(5) === "/") {
+        // you're changing to the parent folder.
+      } else if (string.charAt(5) === "..") {
+        // you're jumping up a folder level
+      } else {
+        // string.charAt(5) is the beginning of the folder name.
       }
+    } else if (string.charAt(2) === "l" && string.charAt(3) === "s") {
+      // the next few files are directories inside the current folder.
+    }
+  } else if (string.charAt(0) === "d" && string.charAt(1) === "i" && string.charAt(2) === "r") {
+    // string.charAt(5) is the start of a directory name
+  } else if (typeof string.charAt(0) === "number") {
+    // string.charAt(5) is the size of a folder OR file
+    // and the word after the number is...
+    // either a filename.extension (with DOT)
+    // or a directory name (without DOT)
+  } else {
+    console.log("Unable to parse output.")
+  }
+
 }
