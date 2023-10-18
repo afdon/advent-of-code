532c94e922843cbcbe64cfc2f37565bebcbb7f5a
afdon <53676753+afdon@users.noreply.github.com>
2023-10-17 22:17:58 -0700

    part 1 done

--- day06/day06-attempt1.ts
+++ day06/day06-attempt1.ts
@@ -24,7 +24,7 @@ function part1(input: string) {
       if (isUnique(store)) {
         marker = store.join()
         markerReady = true
-        charsProcessed = i
+        charsProcessed = i + 1
         console.log("chars processed:", i)
       } else {
         store.shift()
