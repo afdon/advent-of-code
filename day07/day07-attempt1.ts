96b076c42f2305bceae8025140f7f035557ec10d
afdon <53676753+afdon@users.noreply.github.com>
2023-10-18 15:43:16 -0700

    validation works for $ cd /

--- day07/day07-attempt1.ts
+++ day07/day07-attempt1.ts
@@ -1,4 +1,4 @@
-
+// Find all of the directories with a total size of at most 100000. What is the sum of the total sizes of those directories?
 
 import fs from "fs";
 
@@ -7,13 +7,53 @@ fs.readFile("input.txt", "utf8", (err, data) => {
     console.error("Error reading input file:", err);
     return;
   }
-
   part1(data);
 });
 
 function part1(input: string) {
   const lines = input.split("\n");
 
+  let rootContents = listRootContents(lines)
+  console.log(rootContents)
+
+}
+
+const root = {
+  child1: {}
+  child2: {}
+}
+
+// const root = {
+//   name: "/",
+//   children: [child1, child2]
+// }
+
+// const child1 = {
+//   name: "child1",
+//   children: []
+// }
+
+// find the contents of the parent folder.
+
+function listRootContents (lines: string[]): string[] {
+  let rootContents: string[] = []
+  // just assume there's a "$ cd /" command somewhere, followed by "$ ls" on the next line
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i].charAt(0) === "$" && lines[i].charAt(2) === "c" && lines[i].charAt(3) === "d" && lines[i].charAt(5) === "/") {
+      if (lines[i+1].charAt(0) === "$" && lines[i+1].charAt(2) === "l" && lines[i+1].charAt(3) === "s") {
+        // while the next line begins with "dir" or a number, push that line into an array.
+        let j = i + 2
+        console.log(`lines[j] is: ${lines[j]} and its type is:`, typeof lines[j])
+        while (lines[j].charAt(0) !== "$") {
+          console.log(`typeof lines[j]: ${typeof lines[j]}; lines[j]: ${lines[j]}`)
+          rootContents.push(lines[j])
+          j++
+          console.log(`j:`, j)
+        }
+      }
+    }
+  }
+  return rootContents
 }
 
 function validate(string: string) {
@@ -33,10 +73,8 @@ function validate(string: string) {
   } else if (string.charAt(0) === "d" && string.charAt(1) === "i" && string.charAt(2) === "r") {
     // string.charAt(5) is the start of a directory name
   } else if (typeof string.charAt(0) === "number") {
-    // string.charAt(5) is the size of a folder OR file
-    // and the word after the number is...
-    // either a filename.extension (with DOT)
-    // or a directory name (without DOT)
+    // string.charAt(5) is the size of a file
+    // and the word after the number is the file name
   } else {
     console.log("Unable to parse output.")
   }
---
+++ day07/day07-attempt2.ts
@@ -0,0 +1,92 @@
+// Find all of the directories with a total size of at most 100000. What is the sum of the total sizes of those directories?
+
+import fs from "fs";
+
+fs.readFile("input.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading input file:", err);
+    return;
+  }
+  part1(data);
+});
+
+function part1(input: string) {
+  const lines = input.split("\n");
+
+  let rootContents = listRootContents(lines);
+  console.log(rootContents);
+
+  validate(lines[0])
+}
+
+// const root = {
+//   child1: {}
+//   child2: {}
+// }
+
+// const root = {
+//   name: "/",
+//   children: [child1, child2]
+// }
+
+// const child1 = {
+//   name: "child1",
+//   children: []
+// }
+
+// find the contents of the parent folder.
+
+function listRootContents(lines: string[]): string[] {
+  let rootContents: string[] = [];
+  // just assume there's a "$ cd /" command somewhere, followed by "$ ls" on the next line
+  for (let i = 0; i < lines.length; i++) {
+    if (
+      lines[i].charAt(0) === "$" &&
+      lines[i].charAt(2) === "c" &&
+      lines[i].charAt(3) === "d" &&
+      lines[i].charAt(5) === "/"
+    ) {
+      if (
+        lines[i + 1].charAt(0) === "$" &&
+        lines[i + 1].charAt(2) === "l" &&
+        lines[i + 1].charAt(3) === "s"
+      ) {
+        // while the next line begins with "dir" or a number, push that line into an array.
+        let j = i + 2;
+        console.log(
+          `lines[j] is: ${lines[j]} and its type is:`,
+          typeof lines[j]
+        );
+        while (lines[j].charAt(0) !== "$") {
+          console.log(
+            `typeof lines[j]: ${typeof lines[j]}; lines[j]: ${lines[j]}`
+          );
+          rootContents.push(lines[j]);
+          j++;
+          console.log(`j:`, j);
+        }
+      }
+    }
+  }
+  return rootContents;
+}
+
+function validate(string: string) {
+  switch (true) {
+    case /^\$\scd\s\S+/.test(string):
+        if (/^\$\scd\s\//) {
+            console.log("cd /")
+            break
+        } else if (/^\$\scd\s\.\./) {
+            console.log("cd ..")
+            break
+        }
+      console.log("cd into a named directory");
+      break;
+    case /^\$\sls/.test(string):
+      console.log("ls.");
+      break;
+    default:
+      console.log("Unknown");
+  }
+}
