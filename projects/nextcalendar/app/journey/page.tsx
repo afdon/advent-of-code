cbfc0606c9c2280ce6d62b2a6125332e62cbf61f
afdon <afdonovan@alumni.princeton.edu>
2023-11-13 03:23:22 -0700

    save

--- projects/nextcalendar/app/journey/page.tsx
+++ projects/nextcalendar/app/journey/page.tsx
@@ -1,19 +1,43 @@
-import Image from 'next/image'
+import Link from 'next/link'
 import Intro from '../challenges/intro/Intro'
-
+import { Home as HomeIcon, UserCircle, CalendarDays } from 'lucide-react'
 
 export default function Home() {
-  return (
-    <div>
+    return (
+        <div>
 
-      <div style={{ margin: '0 auto' }}>
+            <div
+                className='flex flex-col'
+            >
+                <div className='
+                    flex flex-row gap-4 m-4 justify-between 
+                    xs:hidden 
+                    sm:hidden
+                    md:display-block
+                    lg:display-block
+                    xl:display-block
+                '>
+                    <Link href='/'>Home</Link>
+                    <Link href='/'>Calendar of Code</Link>
+                    <Link href='/'>Your Account</Link>
+                </div>
 
+                <div className='
+                    flex flex-row gap-4 m-4 justify-between 
+                    sm:display-block
+                    md:hidden
+                '>
+                    <Link href='/'>H</Link>
+                    <Link href='/'>Calendar of Code</Link>
+                    <Link href='/'>P</Link>
+                </div>
 
-        <Intro />
+                <Intro />
 
+                <div>Another Thing</div>
 
-      </div>
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }
